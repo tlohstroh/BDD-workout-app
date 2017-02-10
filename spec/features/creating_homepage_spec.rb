7e7ece22d450dd4ddb562ad7e27d76ed99570d80
Tjitske Lohstroh <tjitske@lohstroh.nl>
2017-02-10 11:45:36 +0100

    create home page spec

---
+++ spec/features/creating_homepage_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.feature "Creaing Homepage" do
+
+  visit "/"
+
+
+  # We should see a link called "Home"
+  expect(page).to have_link("Home")
+  # We sould see a link called "Athletes Den"
+  expect(page).to have_link("Athletes Den")
+  # We should see content "Workout Lounge"
+  expect(page).to have_content("Workout Lounge")
+  # We should see content "Show off your workout"
+  expect(page).to have_content("Show off your workout")
+
+  # call the controller DashboardsController
+
+
+end
