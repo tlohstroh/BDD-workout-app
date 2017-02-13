32909c04808ced3d94afcb99f8e0441e41eb1acf
Tjitske Lohstroh <tjitske@lohstroh.nl>
2017-02-13 16:38:22 +0100

    Hide login/ signin links when signed in

--- spec/features/users/signin_user_spec.rb
+++ spec/features/users/signin_user_spec.rb
@@ -15,8 +15,9 @@ RSpec.feature "Sign in users" do
 
     expect(page).to have_content("Signed in successfully")
     expect(page).to have_content("Signed in as tjitske@example.com")
-    # expect(page).not_to have_link("Sign in")
-    # expect(page).not_to have_link("Sign up")
+    expect(page).not_to have_link("Sign out")
+    expect(page).not_to have_link("Sign in")
+    expect(page).not_to have_link("Sign up")
 
   end
 
