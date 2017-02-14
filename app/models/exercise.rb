ea608e5d85ef3ec08940336d06f1f5f927479128
Tjitske Lohstroh <tjitske@lohstroh.nl>
2017-02-14 14:49:47 +0100

    Create exercise unsuccesfully spec

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,13 @@
 class Exercise < ApplicationRecord
   belongs_to :user
+
+  # you can use aliasses if the attributes are called differntly in the database_authenticatable
+  # and you want to prevent confusion....
+  # NOTE this word shows up in your error-messages!
+  alias_attribute :workout_details, :workout
+  alias_attribute :activity_date, :workout_date
+
+  validates :duration_in_min, numericality: { greater_than: 0.0}
+  validates :workout_details, presence: true
+  validates :activity_date, presence: true
 end
