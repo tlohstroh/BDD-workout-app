821879aeb3f84faf8126c0e9306276626f462f3a
Tjitske Lohstroh <tjitske@lohstroh.nl>
2017-02-18 11:16:20 +0100

    create chatroom when user signs up

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,12 @@ class User < ApplicationRecord
   has_many :exercises
   has_many :friendships
   has_many :friends, through: :friendships, class_name: "User"
+  has_one :room
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  after_create :create_chatroom
 
   # works with will_paginate gem to decide how many items per page
   self.per_page = 10
@@ -39,10 +42,18 @@ class User < ApplicationRecord
     friendships.map(&:friend).include?(new_friend) || self == new_friend
 
   end
-  
+
   def current_friendship(friend)
     # returns an ARRAY with one record, so we need the first (and only) one.
     friendships.where(friend: friend).first
+  end
+
+  private
+
+  def create_chatroom
+    hyphenated_username = self.full_name.split.join("-")
+    Room.create(name: hyphenated_username, user_id: self.id)
+
   end
 
 end
